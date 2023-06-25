89bb7a687cc565b9b779917cb3e203cbf91b2f0f
Kayla Lin <kaylalin@Kaylas-MacBook-Pro.local>
2023-06-25 14:57:47 -0700

    added prices

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -3,6 +3,29 @@ import Image from "next/image";
 import { container } from "../../../styled-system/patterns";
 import { forwardRef } from "react";
 
+const LED_PRICES = [
+  {
+    title: "4 LED Panel Light 5CCT (12 PCS)",
+    per: "138 per case ($11.50ea)",
+    bulk: "5 Cases or More: $114 per case ($9.5ea)",
+  },
+  {
+    title: '6" LED Panel Light 5CCT (12 PCS)',
+    per: "144 per case ($12ea)",
+    bulk: "5 Cases or More: $120 per case ($10ea)",
+  },
+  {
+    title: '2"X2" LED Panel Light 3CCT (4 PCS)',
+    per: "$140 per case ($35ea)",
+  },
+  {
+    title: '2"X4" LED Panel Light 3CCT (4 PCS)',
+    per: "$200 per case ($50ea)",
+  },
+];
+
+const priceBulletStyles = css({ listStyleType: "none", fontSize: "sm" });
+
 export const Products = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <>
@@ -38,6 +61,42 @@ export const Products = forwardRef<HTMLDivElement>((_, ref) => {
           </p>
         </div>
       </section>
+      <div
+        className={cx(
+          css({
+            display: "flex",
+            justifyContent: "space-around",
+          }),
+          container()
+        )}
+      >
+        {LED_PRICES.map((price, idx) => (
+          <div
+            key={idx}
+            className={css({
+              margin: "0 auto",
+              marginBottom: "100px",
+              borderStyle: "solid",
+              borderWidth: "1px",
+              borderColor: "primary",
+              color: "primary",
+              display: "flex",
+              flexDir: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "50px 20px",
+            })}
+          >
+            <h3 className={css({ fontSize: "md", fontWeight: "bold" })}>
+              {price.title}
+            </h3>
+            <li className={priceBulletStyles}>{price.per}</li>
+            {price?.bulk && (
+              <li className={priceBulletStyles}>{price?.bulk}</li>
+            )}
+          </div>
+        ))}
+      </div>
       <div ref={ref}>
         <iframe
           className={cx(
